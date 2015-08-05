704eca5f85c9d599eefc2f83ad6707f55bfb6e10
Ahmed Hassan <a.hassan@buismed.de>
2015-08-05 14:26:18 +0200

    corrected styling

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'nerdmed:ios-popover',
-    version: '0.0.2',
+    version: '0.0.3',
 
     // Brief, one-line summary of the package.
     summary: 'A simple ios style popover',
