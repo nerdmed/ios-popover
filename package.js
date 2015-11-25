0e0186da56d83e7e1d83255a1db30c87e273b48f
Geordie <geojay@gmail.com>
2015-11-25 12:46:19 +1000

    Update for Meteor 1.2.1

--- package.js
+++ package.js
@@ -1,21 +1,13 @@
 Package.describe({
     name: 'nerdmed:ios-popover',
-    version: '0.0.5',
-
-    // Brief, one-line summary of the package.
+    version: '0.1.0',
     summary: 'A simple ios style popover',
-
-    // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/nerdmed/ios-popover.git',
-
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+    git: 'https://github.com/nerdmed/ios-popover.git'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.1.0.2');
-    api.use(['grigio:babel@0.1.6', 'templating'],'client');
+    api.versionsFrom('1.2.1');
+    api.use(['ecmascript', 'templating'],'client');
     api.use('less');
     api.addFiles(['popover.html', 'popover.es6.js', 'popover.less'], 'client');
     api.export('IosPopover', 'client');
