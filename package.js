fc78216124199ba2e479dfeed31539d124e60ddb
Ahmed Hassan <a.hassan@buismed.de>
2016-05-01 14:17:13 +0200

    bump version 0.1.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'nerdmed:ios-popover',
-    version: '0.1.1',
+    version: '0.1.2',
     summary: 'A simple ios style popover',
     git: 'https://github.com/nerdmed/ios-popover.git'
 });
